c0f107368796faed75cff209f300e05d2262cf5b
JuhoKaa <jai.kaartinen@gmail.com>
2017-06-20 19:49:14 +0300

    WIP Entities

--- engine/include/managers/Entity.h
+++ engine/include/managers/Entity.h
@@ -3,27 +3,34 @@
 
 //STL includes:
 #include <string>
-#include <vector>
+#include <unordered_map>
+#include <memory>
 
 
 //SE includes:
 #include <utility/Typedefs.h>
+#include <components/Component.h>
 
 namespace se
 {
+//Component typing.
+//For each component, there is a 'id' (name) with which
+//user can find entity's component
+//e.g. GetComponent(COMPONENT_TYPE_TRANSFORM)
+
+
 class Entity
 {
 public:
-	///Default constructor
-	Entity();
+	///Default constructor. Takes in std::string as name of the entity
+	Entity(std::string entityName);
 	///Build Entity from json file
-	Entity(std::string filepath, std::string entityName);
-	///Copy constructors and assign operator
+	Entity(std::string templateName, std::string entityName);
+	///Copy constructors
 	Entity(const Entity&);
-	Entity& operator=(const Entity&);
+	///Assign operator deleted
+	Entity& operator=(const Entity&) = delete;
 
-	///Container holding component id's
-	std::vector<SEuint> myComponents;
 
 	///Static method for adding components to entity
 	static void AddComponent();
@@ -31,6 +38,19 @@ public:
 	///Static method for removing component from entity
 	static void RemoveComponent();
 
+	///Static method for replacing component with another of same type
+	static void ReplaceComponent();
+
+	///Static method for getting component. Returns pointer to component.
+	static std::shared_ptr<priv::Component> GetComponent(/*COMPONENT_TYPE type*/);
+
+	///Name of the entity
+	const std::string name;
+
+private:
+	///Container holding component id's that can be found with right name.
+	///Uses unordered_map for quick look-ups, addition and removals (unordered_map uses std::hash internally to create key-value pairs)
+	std::unordered_map<std::string, std::shared_ptr<priv::Component>> m_myComponents;
 };
 }//namespace se
 
--- engine/src/managers/Entity.cpp
+++ engine/src/managers/Entity.cpp
@@ -2,24 +2,22 @@
 
 namespace se
 {
-Entity::Entity()
+Entity::Entity(std::string name)
+	: name(name)
 {
 
 }
 
 Entity::Entity(std::string filepath, std::string entityName)
+	: name(entityName)
 {
 
 }
 
 Entity::Entity(const Entity& other)
+	:name(other.name)
 {
 
-}
-
-Entity& Entity::operator=(const Entity& other)
-{
-	return *this;
 }
 
 void Entity::AddComponent()
@@ -30,6 +28,16 @@ void Entity::AddComponent()
 void Entity::RemoveComponent()
 {
 
+}
+
+void Entity::ReplaceComponent()
+{
+
+}
+
+std::shared_ptr<priv::Component> Entity::GetComponent(/*COMPONENT_TYPE type*/)
+{
+	//Use COMPONENT_TYPE and entity's name to make a unigue identifier
 }
 
 }//namespace se
