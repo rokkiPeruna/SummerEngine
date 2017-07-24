b4042d62ba90add93916125e61e27f2d890605b2
KAMIT\juhokaa <juhokaartinen>
2017-07-24 10:57:51 +0300

    merge resolution

--- engine/include/systems/AnimationSystem.h
+++ engine/include/systems/AnimationSystem.h
@@ -1,6 +1,9 @@
 #ifndef SE_ANIMATION_SYSTEM_H
 #define SE_ANIMATION_SYSTEM_H
 
+//STL includes:
+#include <unordered_map>
+
 //SE includes:
 #include <systems/ComponentSystem.h>
 #include <components/CTexture.h>
@@ -43,7 +46,7 @@ public:
 
 	/*-----------------EDITOR METHODS--------------------*/
 
-	SEuint CreateComponent(Entity& entity, COMPONENT_TYPE component_type, SceneFileFormatIterator& entity_obj) override final;
+	SEint CreateComponent(Entity& entity, COMPONENT_TYPE component_type, SceneFileFormatIterator& entity_obj) override final;
 
 	void RemoveComponent(Entity& entity, COMPONENT_TYPE component_type, SceneFileFormatIterator& entity_obj) override final;
 
@@ -58,12 +61,21 @@ private:
 	///Resource manager ptr for image loading
 	ResourceManager* m_res_mgr;
 
-	///Create texture
+	///Unordered map holding texture ids. Key is std::string as name of the texture with suffix (e.g. tiles.png),
+	///value is texture id.
+	std::unordered_map<std::string, SEuint> m_texture_map;
 
+	///Create texture.
+	///1.param: Name of the texture with correct suffix (e.g. player.png)
+	///--
+	///Returns handle (SEint) to that texture to be used by OpenGL and shaders. 
+	SEuint _createTexture(const std::string& texture_name, CTexture& tex_component);
+	
 	///Assing texture
 
 	///OLET TÄSSÄ
 	SEint remove;
+	SEint tama_on_vittu_change;
 
 };
 
